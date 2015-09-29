a8d001790c0d1abd5a43d70fff1ddb3db63d825c
Aapo Talvensaari <aapo.talvensaari@gmail.com>
2015-09-29 13:13:48 +0300

    Fixes issue #1

--- lib/resty/prettycjson.lua
+++ lib/resty/prettycjson.lua
@@ -8,13 +8,13 @@ return function(tbl, lf, id, ac)
     local s, e = json(tbl)
     if not s then return s, e end
     lf, id, ac = lf or "\n", id or "\t", ac or " "
-    local i, j, k, r, p  = 1, 0, 0, {}, nil
+    local i, j, k, r, p, q  = 1, 0, 0, {}, nil, nil
     local al = sub(ac, -1) == "\n"
     for c in gmatch(s, ".") do
-        if c == "{" or c == "[" then
+        if not q and (c == "{" or c == "[") then
             r[i] = p == ":" and concat{ c, lf } or concat{ rep(id, j), c, lf }
             j = j + 1
-        elseif c == "}" or c == "]" then
+        elseif not q and (c == "}" or c == "]") then
             j = j - 1
             if p == "{" or p == "[" then
                 i = i - 1
@@ -22,16 +22,25 @@ return function(tbl, lf, id, ac)
             else
                 r[i] = concat{ lf, rep(id, j), c }
             end
-        elseif c == "," then
+        elseif not q and c == "," then
             r[i] = concat{ c, lf }
             k = -1
-        elseif c == ":" then
+        elseif not q and c == ":" then
             r[i] = concat{ c, ac }
             if al then
                 i = i + 1
                 r[i] = rep(id, j)
             end
         else
+            if c == '"' or c == "'" then
+                if p ~= "\\" then
+                    if q == c then
+                        q = nil
+                    elseif not q then
+                        q = c
+                    end
+                end
+            end
             if j ~= k then
                 r[i] = rep(id, j)
                 i, k = i + 1, j
